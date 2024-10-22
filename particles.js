8e2552559b1bde4a292e690c18c1ba27d296bfe1
Mahdiyar Molahasani <45335894+MahdiyarMM@users.noreply.github.com>
2024-10-22 02:41:33 -0400

    Update particles.js

--- particles.js
+++ particles.js
@@ -33,7 +33,7 @@ var pJS = function(tag_id, params){
         type: 'circle',
         stroke: {
           width: 0,
-          color: '#000000'
+          color: '#ffffff'
         },
         polygon: {
           nb_sides: 5
@@ -67,7 +67,7 @@ var pJS = function(tag_id, params){
       line_linked: {
         enable: true,
         distance: 100,
-        color: '#000000',
+        color: '#ffffff',
         opacity: 1,
         width: 1
       },
